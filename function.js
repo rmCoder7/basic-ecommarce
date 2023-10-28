212ae079890793439b885c06bac044e0d55abe1d
rmCoder7 <mohammedayon2021@gmail.com>
2023-10-28 11:39:07 +0600

    add

--- function.js
+++ function.js
@@ -1,7 +1,22 @@
+let totalamount = 0;
+
 function handler(data){
+// console.log(data.parentNode.childNodes[1].innerText);
+
 const Uldiv = document.getElementById('update-div');
 const itemText = data.parentNode.childNodes[1].innerText
 const ulupdate = document.getElementById('ul-id');
 ulupdate.innerText = itemText 
 Uldiv.appendChild(ulupdate)
+
+
+// price section
+
+// console.log(data.parentNode.childNodes[3].innerText.split(" ")[0]);
+const price = data.parentNode.childNodes[3].innerText.split(" ")[0]
+totalamount = parseInt(totalamount)+parseInt(price);
+ const totallprice = document.getElementById("total-price").innerText=totalamount;
+
+
+
 }
